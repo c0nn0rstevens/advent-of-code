3dc37299a1982de6fcb111792f2eb69fc520727d
c0nn0rstevens <72092369+c0nn0rstevens@users.noreply.github.com>
2024-01-03 00:18:44 +0100

    Add boilerplate.

--- aoc-2023/python/day-1/part-1.py
+++ aoc-2023/python/day-1/part-1.py
@@ -1,41 +1,45 @@
 import os
 
+def main():
+    script_dir = os.path.dirname(__file__)
+    rel_path = "input.txt"
+    abs_file_path = os.path.join(script_dir, rel_path)
 
-script_dir = os.path.dirname(__file__)
-rel_path = "input.txt"
-abs_file_path = os.path.join(script_dir, rel_path)
+    # List of digits for comparison.
+    DIGITS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
 
-# List of digits for comparison.
-DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    # Read input file and save separate lines into a list.
+    with open(abs_file_path) as f:
+        lines = []
+        for line in f:
+            lines.append(line)
 
-# Read input file and save separate lines into a list.
-with open(abs_file_path) as f:
-    lines = []
-    for line in f:
-        lines.append(line)
+    calibration_values = []
 
-calibration_values = []
+    for line in lines:
+        # print('Line: ' + line)
+        # Iterate over each character in the string.
+        for char in line:
+            if char in DIGITS:
+                first_digit = char
+                # print('First number: ' + first_digit)
+                break
+            else:
+                continue
 
-for line in lines:
-    # print('Line: ' + line)
-    # Iterate over each character in the string.
-    for char in line:
-        if char in DIGITS:
-            first_digit = char
-            # print('First number: ' + first_digit)
-            break
-        else:
-            continue
+        for char in reversed(line):
+            if char in DIGITS:
+                second_digit = char
+                # print('Second number: ' + second_digit + '\n')
+                break
+            else:
+                continue
 
-    for char in reversed(line):
-        if char in DIGITS:
-            second_digit = char
-            # print('Second number: ' + second_digit + '\n')
-            break
-        else:
-            continue
+        calibration_values.append(int(first_digit + second_digit))
 
-    calibration_values.append(int(first_digit + second_digit))
+    calibration_sum = sum(calibration_values)
+    print(calibration_sum)
 
-calibration_sum = sum(calibration_values)
-print(calibration_sum)
+
+if __name__ == "__main__":
+    main()
