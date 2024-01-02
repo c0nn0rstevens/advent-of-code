97b3777b7027a26921f5cc3281e8daae0b9c2c27
c0nn0rstevens <72092369+c0nn0rstevens@users.noreply.github.com>
2024-01-02 23:02:31 +0100

    Add markdown table printout of benchmark statistics.

--- benchmarker/src/main.py
+++ benchmarker/src/main.py
@@ -6,6 +6,11 @@ given solution.
 
 import sys
 from benchmarker import benchmark
+import statistics
+import datetime
+from py_markdown_table.markdown_table import markdown_table
+
+PRECISION = 5
 
 
 def main():
@@ -13,8 +18,33 @@ def main():
     testee = sys.argv[1]
     runner = sys.argv[2]
     n = int(sys.argv[3])
-    duration = benchmark(runner, testee, n)
-    print(duration)
+    durations = benchmark(runner, testee, n)
+    lower_quartile = statistics.median_low(durations)
+    minimum = min(durations)
+    maximum = max(durations)
+    mean = statistics.mean(durations)
+    median = statistics.median(durations)
+    mode = statistics.mode(durations)
+    upper_quartile = statistics.median_high(durations)
+    std_dev = statistics.stdev(durations)
+    today = datetime.date.today().strftime("%d/%m/%Y")
+    data = [
+        {
+            "mean": f'{mean:.{PRECISION}}',
+            "mode": f'{mode:.{PRECISION}}',
+            "min": f'{minimum:.{PRECISION}}',
+            "q1": f'{lower_quartile:.{PRECISION}}',
+            "median": f'{median:.{PRECISION}}',
+            "q3": f'{upper_quartile:.{PRECISION}}',
+            "max": f'{maximum:.{PRECISION}}',
+            "std dev": f'{std_dev:.{PRECISION}}'
+        }
+    ]
+    md_table = markdown_table(data).get_markdown()
+    # Get rid of codeblock backticks.
+    md_table = md_table[3:-3]
+    markdown_template = f"""Date: {today}{md_table}"""
+    print(markdown_template)
 
 
 if __name__ == "__main__":
